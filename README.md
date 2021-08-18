23375dd91214e235852da1e03e920da212ad857e
Hlamulo-Mav <zhlamulo@gmail.com>
2021-08-18 23:47:24 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # PythonFunctions
+
+Basic python functions
+
+## Built With
+- Python
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+- Clone or download this repository to your local workstation.
+
+### Prerequisites
+- Text Editor 
+
+### Usage
+- Enter the directory where you cloned or downloaded the project and open index.html file using your favorite browser
+
+## Authors
+:bust_in_silhouette: **Hlamulo**
+- Github: [@Hlamulo-Mav](https://github.com/Hlamulo-Mav)  
+
+## :handshake: Contributing
+Contributions, issues and feature requests are welcome! <br />
+Feel free to check the [issues page](https://github.com/Hlamulo-Mav/PythonFunctions/issues)
+
+## Show your support
+
+kindly give this repository a :star: if you like the project!
+
+## Acknowledgements
+- Girlcode Team  
+
+## License
+
+This project is [MIT](https://github.com/Hlamulo-Mav/PythonFunctions/blob/main/LICENSE) licensed.
---
+++ index.py
@@ -0,0 +1,66 @@
+#PYTHON FUNCTIONS
+
+
+# function to check if string is palindrome or not
+
+def isPalindrome(word):
+    #  Using inBuilt function to reverse and join a string
+    # Checking if both string are equal or not
+    rev_word = ''.join(reversed(word))
+
+    if (word == rev_word):
+        print("The word is a palindrome")
+    else:
+        print("The word is not a Palindrome")
+isPalindrome("madam")
+isPalindrome("dad")
+
+
+# Function that takes an array of numbers and returns a new array where every element of the original array is multiplied by 2
+def doubler(arr):
+    # Initiase an Empty array to append to later
+    # loops through the length of the array and multiply each element by 2
+    #append and increment by 1, the return the Output
+  doubled_arr = []
+  i = 0
+  while i < len(arr) :
+    Input_arr = arr[i]
+    Output_arr = Input_arr* 2
+    doubled_arr.append(Output_arr)
+
+    i += 1
+  return doubled_arr
+
+print(doubler([2, 4, 3]))
+
+
+# Function that returns the average of three numbers
+def average(n1, n2, n3): 
+	total = int(n1) + int(n2) + int(n3) 
+	Avg = total/3 
+	print(Avg)
+
+average(1, 2, 3)
+
+# Function that takes in a string name and returns a string saying bye to that name
+
+def Goodbye(name):
+    str_name = "Goodbye" + " " + name
+
+    return str_name
+
+print(Goodbye("Zaza"))
+
+# that takes in a number max and returns an array containing all numbers greater than 0 and less than max that are divisible by either 4 or 6, but not both
+def fizz_buzz(max):
+    nums = []
+
+    for i in range(0, max):
+        if((i % 4 == 0 or i % 6 == 0) and  (i % 4 != 0 or  i % 6 != 0)):
+            nums.append(i)
+
+            i += 1
+    return(nums)
+
+print(fizz_buzz(15))
+print(fizz_buzz(20))
